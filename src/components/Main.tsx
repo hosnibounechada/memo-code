0190b684004fe13e4d0ec17536bf1ec441e3bb37
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-08-08 18:49:57 +0100

    refactor plugins usage

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import { useEffect, useState, useRef } from "react";
 import { unpkgPathPlugin } from "../plugins/unpkg-path-plugin";
+import { fetchPlugin } from "../plugins/fetch-plugin";
 
 const Main : React.FC = () => {
     const ref = useRef<any>();
@@ -10,7 +11,7 @@ const Main : React.FC = () => {
     const startService = async() => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: "/esbuild.wasm"
+            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
         });
     }
 
@@ -25,14 +26,13 @@ const Main : React.FC = () => {
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()],
+            plugins: [unpkgPathPlugin(),fetchPlugin(input)],
             define: {
                 'process.env.NODE_ENV' : '"production"',
                 global: 'window'
             }
 
-        })
-        console.log(result); 
+        }) 
         setCode(result.outputFiles[0].text);
     }
 
