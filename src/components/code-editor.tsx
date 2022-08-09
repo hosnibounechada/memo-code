0dbd282d9aee423caaf88f94e8eafd5454487a9f
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-08-09 02:38:19 +0100

    monaco/react editor

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,70 @@
+import "./code-editor.css";
+import { useRef } from "react";
+import MonacoEditor, { OnChange, OnMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>();
+
+  const handleEditorChange: OnChange = (value) => {
+    if (value) onChange(value);
+  };
+
+  const handleEditorMount: OnMount = (editor) => {
+    editorRef.current = editor;
+  };
+
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue();
+
+    const formatted = prettier
+      .format(unformatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, "");
+
+    editorRef.current.setValue(formatted);
+  };
+
+  return (
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        defaultValue={initialValue}
+        onChange={handleEditorChange}
+        onMount={handleEditorMount}
+        theme="vs-dark"
+        language="javascript"
+        height="500px"
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: true,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          tabSize: 2,
+        }}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
