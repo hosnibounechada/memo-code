890f3b5091dba9540599146ec97f5b9cc9e1c3ad
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-08-09 02:43:21 +0100

    refactor Code cells Main Component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,69 +1,12 @@
-import * as esbuild from "esbuild-wasm";
-import { useEffect, useState, useRef } from "react";
-import { unpkgPathPlugin } from "../plugins/unpkg-path-plugin";
-import { fetchPlugin } from "../plugins/fetch-plugin";
+import CodeCell from "./code-cell";
 
-const Main : React.FC = () => {
-    const ref = useRef<any>();
-    const iframe = useRef<any>();
-    const [input, setInput] = useState("");
-
-    const startService = async() => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
-        });
-    }
-
-    useEffect(() =>{
-        startService();
-    },[]);
-
-    const onClick = async () => {
-        if(!ref.current) return;
-
-        iframe.current.srcdoc = html;
-
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(),fetchPlugin(input)],
-            define: {
-                'process.env.NODE_ENV' : '"production"',
-                global: 'window'
-            }
-
-        }) 
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
-    }
-
-    const html = `
-    <html>
-    <head></head>
-    <body>
-    <div id="root"></div>
-    <script>
-    window.addEventListener('message', (event) => {
-        try{
-            eval(event.data);
-        }catch(err){
-            const root = document.querySelector('#root');
-            root.innerHTML = '<div style="color: red;"><h4>Runtime error</h4>' + err +'</div>';
-        }
-    }, false);
-    </script>
-    </body>
-    </html>
-    `;
-
-    return <div>
-        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
-        <div>
-            <button onClick={onClick}>Submit</button>
-        </div>
-        <iframe ref={iframe} title="preview" sandbox="allow-scripts" srcDoc={html}/>
+const Main: React.FC = () => {
+  return (
+    <div>
+      <CodeCell />
+      <CodeCell />
     </div>
-}
+  );
+};
 
 export default Main;
